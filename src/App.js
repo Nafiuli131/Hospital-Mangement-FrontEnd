3cae4f1e777f956ea388fd739810835aa2f77faf
Nafiul Islam <nafiul.islam@synesisit.info>
2022-07-12 19:46:30 +0600

    doctor signin api and connection to the backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LandPage from "./LandPage/LandPage.tsx";
-
 function App() {
   return (
     <div className="App">
--- src/Doctor/DoctorSignIn.tsx
+++ src/Doctor/DoctorSignIn.tsx
@@ -1,5 +1,48 @@
 import styles from "../Doctor/Doctor.module.css";
+import { useState } from "react";
+
 export default function DoctorSignIn() {
+  const [dname, setdName] = useState<any>();
+  const [dqualification, setdQualification] = useState<any>();
+  const [darea, setDarea] = useState<any>();
+  const [demail, setdEmail] = useState<any>();
+  const [dPass, setdPass] = useState<any>();
+  const axios = require("axios");
+  const saveUrl = "http://localhost:8080/saveDoctor";
+  function name(e) {
+    setdName(e.target.value);
+  }
+  function qualification(e) {
+    setdQualification(e.target.value);
+  }
+  function area(e) {
+    setDarea(e.target.value);
+  }
+  function email(e) {
+    setdEmail(e.target.value);
+  }
+  function password(e) {
+    setdPass(e.target.value);
+  }
+  function postSubmit() {
+    let doctorInfo;
+    doctorInfo = {
+      d_name: dname,
+      qualification: dqualification,
+      specified_area: darea,
+      email: demail,
+      password: dPass,
+    };
+    axios
+      .post(saveUrl, doctorInfo)
+      .then((response) => {
+        alert("save successfully");
+      })
+      .catch((error) => {
+        alert("don't save successfully");
+      });
+  }
+
   return (
     <div className={styles.Authformcontainer}>
       <form className={styles.Authform}>
@@ -10,11 +53,20 @@ export default function DoctorSignIn() {
           </div>
           <div className="form-group mt-3">
             <label>Name </label>
-            <input className="form-control mt-1" placeholder="Enter Name" />
+            <input
+              onChange={(e) => {
+                name(e);
+              }}
+              className="form-control mt-1"
+              placeholder="Enter Name"
+            />
           </div>
           <div className="form-group mt-3">
             <label>Qualification </label>
             <input
+              onChange={(e) => {
+                qualification(e);
+              }}
               className="form-control mt-1"
               placeholder="Enter Qualification"
             />
@@ -22,6 +74,9 @@ export default function DoctorSignIn() {
           <div className="form-group mt-3">
             <label>Specialized Area </label>
             <input
+              onChange={(e) => {
+                area(e);
+              }}
               className="form-control mt-1"
               placeholder="Enter Specialized Area "
             />
@@ -29,6 +84,9 @@ export default function DoctorSignIn() {
           <div className="form-group mt-3">
             <label>Email </label>
             <input
+              onChange={(e) => {
+                email(e);
+              }}
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
@@ -37,17 +95,19 @@ export default function DoctorSignIn() {
           <div className="form-group mt-3">
             <label>Password</label>
             <input
+              onChange={(e) => {
+                password(e);
+              }}
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button onClick={postSubmit} className="btn btn-primary">
               Submit
             </button>
           </div>
-          {/* <div className="forgot-password text-right mt-2">Forget Password</div> */}
         </div>
       </form>
     </div>
